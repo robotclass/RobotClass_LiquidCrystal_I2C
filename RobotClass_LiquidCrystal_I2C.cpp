9ba28da1fe4c8236e7df011c2a123e050c8a4365
oevsegneev <oleg.evsegneev@gmail.com>
2025-01-25 23:07:42 +0500

    [+] убрана зависимость от LiquidCrystal_LCD [+] добавлена поддержка PCA9534. В конструктор добавлен параметр io, который может принимать два значения: IO_MODEL_PCA9534 (по ум.), IO_MODEL_PCF8574

--- RobotClass_LiquidCrystal_I2C.cpp
+++ RobotClass_LiquidCrystal_I2C.cpp
@@ -1,17 +1,26 @@
 /*
- * LiquidCrystal_I2C library override for cyrillic LCD
- * Written by Oleg Evsegneev for RobotClass (2022).
+ * LiquidCrystal_I2C - библиотека для символьных ЖК дисплеев (Hitachi HD44780) со встроенным I2C интерфейсом
+ * поддержка кириллицы
+ * Автор Олег Евсегнеев для RobotClass (2025).
  * e-mail: oleg.evsegneev@gmail.com
  */
 #include "RobotClass_LiquidCrystal_I2C.h"
 
-// overrides LiquidCrystal converter
-RobotClass_LiquidCrystal_I2C::RobotClass_LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t cp): LiquidCrystal_I2C(lcd_Addr, lcd_cols, lcd_rows){
-  codepage = cp;
+#include "Arduino.h"
+
+RobotClass_LiquidCrystal_I2C::RobotClass_LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t cp, uint8_t io){
+    _codepage = cp;
+    _io = io;
+    
+    _Addr = lcd_Addr;
+    _cols = lcd_cols;
+    _rows = lcd_rows;
+    _backlightval = LCD_NOBACKLIGHT;
 }
 
-// converts utf-8 string to cp1251
-// author V. Kirievskiy - vek2006_73@@mail.ru
+/*-------- Поддержка кириллицы --------*/
+
+// преобразование строк utf-8 в cp1251
 String RobotClass_LiquidCrystal_I2C::utf8rus(String source){
   int i,k;
   String target;
@@ -44,26 +53,26 @@ String RobotClass_LiquidCrystal_I2C::utf8rus(String source){
   return target;
 }
 
-// overrides print function from core Print module
-// prints String with on-fly conversion to LCD codepage
+// перегрузка функции print из модуля Print
+// выводит строки с преобразованием из кодировки IDE в кодировку дисплея
 size_t RobotClass_LiquidCrystal_I2C::print( const String &s ){
-  if( codepage == CP_UTF8 ){
-    return LiquidCrystal_I2C::print( convertCyr( utf8rus( s )));
-  } else {
-    return LiquidCrystal_I2C::print( convertCyr( s ));
-  }
+    if( _codepage == CP_UTF8 ){
+        return Print::print( convertCyr( utf8rus( s )));
+    } else {
+        return Print::print( convertCyr( s ));
+    }
 }
 
-// converts cp1251 String to LCD codepage String
+// преобразует строку из кодировки IDE в кодировку дисплея
 String RobotClass_LiquidCrystal_I2C::convertCyr( const String &s ){
-  String target = s;
-  for( int idx = 0; idx<s.length(); idx++ ){
-    target[idx] = getCharCyr( s[idx] );
-  }
-  return target;
+    String target = s;
+    for( int idx = 0; idx<s.length(); idx++ ){
+        target[idx] = getCharCyr( s[idx] );
+    }
+    return target;
 }
 
-// converts cp1251 Char to LCD codepage Char
+// преобразует сивол из кодировки IDE в кодировку дисплея
 uint8_t RobotClass_LiquidCrystal_I2C::getCharCyr( uint8_t ch ){
   char rch = ch;
   switch (ch){
@@ -139,4 +148,220 @@ uint8_t RobotClass_LiquidCrystal_I2C::getCharCyr( uint8_t ch ){
     case 0xB8: rch = 0xB5; break;
   }
   return rch;
+}
+
+void RobotClass_LiquidCrystal_I2C::init(){
+    Wire.begin();
+    if( _io == IO_MODEL_PCA9534 ){
+        expanderWrite(IO_REG_CONFIG, 0x00);
+    }
+    
+    _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+    begin(_cols, _rows);  
+}
+
+void RobotClass_LiquidCrystal_I2C::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
+    if (lines > 1) {
+        _displayfunction |= LCD_2LINE;
+    }
+    _numlines = lines;
+
+    // for some 1 line displays you can select a 10 pixel high font
+    if ((dotsize != 0) && (lines == 1)) {
+        _displayfunction |= LCD_5x10DOTS;
+    }
+
+    // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
+    // according to datasheet, we need at least 40ms after power rises above 2.7V
+    // before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
+    delay(50); 
+  
+    // Now we pull both RS and R/W low to begin commands
+    expanderWrite(IO_REG_OUTPUT, _backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
+    delay(1000);
+
+    //put the LCD into 4 bit mode
+    // this is according to the hitachi HD44780 datasheet
+    // figure 24, pg 46
+	
+    // переход в 4-разрядный режим
+    write4bits(0x03 << 4);
+    delayMicroseconds(4500); // wait min 4.1ms
+   
+    // попытка 2
+    write4bits(0x03 << 4);
+    delayMicroseconds(4500); // wait min 4.1ms
+   
+    // попытка 3
+    write4bits(0x03 << 4); 
+    delayMicroseconds(150);
+   
+    // установка 4-разрядного режима
+    write4bits(0x02 << 4); 
+
+
+    // настройка отображения
+    command(LCD_FUNCTIONSET | _displayfunction);  
+	
+    // включение дисплея без курсора
+    _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+    display();
+	
+    // очистка дисплея
+    clear();
+	
+    // настройка направления текста
+    _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+	
+    // set the entry mode
+    command(LCD_ENTRYMODESET | _displaymode);
+	
+    home();
+}
+
+/*-------- Методы высокого уровня --------*/
+
+// очистка дисплея, перевод курсора в нулевую позицию
+void RobotClass_LiquidCrystal_I2C::clear(){
+    command(LCD_CLEARDISPLAY);
+    delayMicroseconds(2000);
+}
+
+// перевод курсора в нулевую позицию
+void RobotClass_LiquidCrystal_I2C::home(){
+    command(LCD_RETURNHOME);
+    delayMicroseconds(2000);
+}
+
+// перевод курсора в позицию col, row
+void RobotClass_LiquidCrystal_I2C::setCursor(uint8_t col, uint8_t row){
+    int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
+    if ( row > _numlines ) {
+        row = _numlines-1;    // we count rows starting w/0
+    }
+    command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
+}
+
+// включение/выключение дисплея
+void RobotClass_LiquidCrystal_I2C::noDisplay() {
+    _displaycontrol &= ~LCD_DISPLAYON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+void RobotClass_LiquidCrystal_I2C::display() {
+    _displaycontrol |= LCD_DISPLAYON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// включение/выключение подчерка под курсором
+void RobotClass_LiquidCrystal_I2C::noCursor() {
+    _displaycontrol &= ~LCD_CURSORON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+void RobotClass_LiquidCrystal_I2C::cursor() {
+    _displaycontrol |= LCD_CURSORON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// включение/выключение мигающего курсора
+void RobotClass_LiquidCrystal_I2C::noBlink() {
+    _displaycontrol &= ~LCD_BLINKON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+void RobotClass_LiquidCrystal_I2C::blink() {
+    _displaycontrol |= LCD_BLINKON;
+    command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// перемотка дисплея без изменения памяти
+void RobotClass_LiquidCrystal_I2C::scrollDisplayLeft(void) {
+    command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
+}
+void RobotClass_LiquidCrystal_I2C::scrollDisplayRight(void) {
+    command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
+}
+
+// направление текста слева направо
+void RobotClass_LiquidCrystal_I2C::leftToRight(void) {
+    _displaymode |= LCD_ENTRYLEFT;
+    command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// направление текста справа налево
+void RobotClass_LiquidCrystal_I2C::rightToLeft(void) {
+    _displaymode &= ~LCD_ENTRYLEFT;
+    command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// выравнивание текста справа
+void RobotClass_LiquidCrystal_I2C::autoscroll(void) {
+    _displaymode |= LCD_ENTRYSHIFTINCREMENT;
+    command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// выравнивание текста слева
+void RobotClass_LiquidCrystal_I2C::noAutoscroll(void) {
+    _displaymode &= ~LCD_ENTRYSHIFTINCREMENT;
+    command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// Перезапись первых 8 CGRAM (символов)
+void RobotClass_LiquidCrystal_I2C::createChar(uint8_t location, uint8_t charmap[]) {
+    location &= 0x7; // we only have 8 locations 0-7
+    command(LCD_SETCGRAMADDR | (location << 3));
+    for (int i=0; i<8; i++) {
+        write(charmap[i]);
+    }
+}
+
+// Включение/выключение подсветки
+void RobotClass_LiquidCrystal_I2C::noBacklight(void) {
+    _backlightval=LCD_NOBACKLIGHT;
+    expanderWrite(IO_REG_OUTPUT, 0);
+}
+
+void RobotClass_LiquidCrystal_I2C::backlight(void) {
+    _backlightval=LCD_BACKLIGHT;
+    expanderWrite(IO_REG_OUTPUT, 0);
+}
+
+/*-------- Методы низкого уровня --------*/
+
+inline void RobotClass_LiquidCrystal_I2C::command(uint8_t value) {
+    send(value, 0);
+}
+
+inline size_t RobotClass_LiquidCrystal_I2C::write(uint8_t value) {
+	send(value, Rs);
+	return 1;
+}
+
+/*-------- Методы интерфейсов --------*/
+
+void RobotClass_LiquidCrystal_I2C::send(uint8_t value, uint8_t mode) {
+    uint8_t highnib = value & 0xf0;
+    uint8_t lownib = (value << 4) & 0xf0;
+    write4bits( highnib | mode );
+    write4bits( lownib | mode ); 
+}
+
+void RobotClass_LiquidCrystal_I2C::write4bits(uint8_t value) {
+    expanderWrite(IO_REG_OUTPUT, value);
+    pulseEnable(value);
+}
+
+void RobotClass_LiquidCrystal_I2C::expanderWrite(uint8_t reg, uint8_t _data){                                        
+    Wire.beginTransmission(_Addr);
+    if(_io == IO_MODEL_PCA9534){
+        Wire.write(reg);
+    }
+    Wire.write(_data | _backlightval);
+    Wire.endTransmission();   
+}
+
+void RobotClass_LiquidCrystal_I2C::pulseEnable(uint8_t _data){
+    expanderWrite(IO_REG_OUTPUT, _data | En);	// En high
+    delayMicroseconds(1);		// enable pulse must be >450ns
+	
+    expanderWrite(IO_REG_OUTPUT, _data & ~En);	// En low
+    delayMicroseconds(50);		// commands need > 37us to settle
 }
--- RobotClass_LiquidCrystal_I2C.h
+++ RobotClass_LiquidCrystal_I2C.h
@@ -1,21 +1,118 @@
 /*
- * LiquidCrystal_I2C library override for cyrillic LCD
- * Written by Oleg Evsegneev for RobotClass (2022).
+ * LiquidCrystal_I2C - библиотека для символьных ЖК дисплеев (Hitachi HD44780) со встроенным I2C интерфейсом
+ * поддержка кириллицы
+ * Автор Олег Евсегнеев для RobotClass (2025).
  * e-mail: oleg.evsegneev@gmail.com
-*/
+ */
  
-#include <LiquidCrystal_I2C.h>
+#include <inttypes.h>
+#include "Print.h" 
+#include <Wire.h>
+
+#define IO_MODEL_PCA9534 0x01
+#define IO_MODEL_PCF8574 0x02
+
+#define IO_REG_OUTPUT 0x01
+#define IO_REG_CONFIG 0x03
 
 #define CP_UTF8 0
 #define CP_CP1251 1
 
-class RobotClass_LiquidCrystal_I2C: public LiquidCrystal_I2C {
+// commands
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+// flags for function set
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+// flags for backlight control
+#define LCD_BACKLIGHT 0x08
+#define LCD_NOBACKLIGHT 0x00
+
+#define En B00000100  // Enable bit
+#define Rw B00000010  // Read/Write bit
+#define Rs B00000001  // Register select bit
+
+class RobotClass_LiquidCrystal_I2C: public Print {
     public:
-        RobotClass_LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows, uint8_t cp);
+        RobotClass_LiquidCrystal_I2C(uint8_t lcd_Addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t cp = CP_UTF8, uint8_t io = IO_MODEL_PCA9534);
         size_t print( const String &s );
+        void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
+        void clear();
+        void home();
+        void noDisplay();
+        void display();
+        void noBlink();
+        void blink();
+        void noCursor();
+        void cursor();
+        void scrollDisplayLeft();
+        void scrollDisplayRight();
+        void printLeft();
+        void printRight();
+        void leftToRight();
+        void rightToLeft();
+        void shiftIncrement();
+        void shiftDecrement();
+        void noBacklight();
+        void backlight();
+        void autoscroll();
+        void noAutoscroll(); 
+        void createChar(uint8_t, uint8_t[]);
+        void setCursor(uint8_t, uint8_t); 
+        virtual size_t write(uint8_t);
+        void command(uint8_t);
+        void init();
+
     private:
-        uint8_t codepage;
         String utf8rus(String source);
         String convertCyr( const String &s );
         uint8_t getCharCyr( uint8_t ch );
+
+        void send(uint8_t, uint8_t);
+        void write4bits(uint8_t);
+        void expanderWrite(uint8_t, uint8_t);
+        void pulseEnable(uint8_t);
+
+        uint8_t _codepage;
+        uint8_t _io;
+        uint8_t _Addr;
+        uint8_t _displayfunction;
+        uint8_t _displaycontrol;
+        uint8_t _displaymode;
+        uint8_t _numlines;
+        uint8_t _cols;
+        uint8_t _rows;
+        uint8_t _backlightval;
 };
