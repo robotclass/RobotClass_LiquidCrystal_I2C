13c2e7c8f3497568503b53f2c4319ad19acf47d3
oevsegneev <oleg.evsegneev@gmail.com>
2025-08-17 21:16:24 +0500

    [+] I2C адрес в примере заменен на 0x3F, примёр упрощён

--- examples/CyrillicLCDTest/CyrillicLCDTest.ino
+++ examples/CyrillicLCDTest/CyrillicLCDTest.ino
@@ -1,24 +1,18 @@
 #include <RobotClass_LiquidCrystal_I2C.h>
 
-RobotClass_LiquidCrystal_I2C lcd(0x27, 16, 2, CP_UTF8);
+RobotClass_LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 byte idx = 0;
 
 void setup() {
   lcd.init();
   lcd.backlight();
+
+  lcd.setCursor(0,0);
+  lcd.print( "Привет!" );
+  lcd.setCursor(0,1);
+  lcd.print( "мир" );
 }
 
 void loop() {
-  lcd.setCursor(0,0);
-  lcd.print( "АБВГДЕЁЖЗИКЛМНОП" );
-  lcd.setCursor(0,1);
-  lcd.print( "абвгдеёжзиклмноп" );
-  delay(1000);
-
-  lcd.setCursor(0,0);
-  lcd.print( "РСТУФХЦЧШЩЪЫЬЭЮЯ" );
-  lcd.setCursor(0,1);
-  lcd.print( "рстуфхцчшщъыьэюя" );
-  delay(1000);
 }
